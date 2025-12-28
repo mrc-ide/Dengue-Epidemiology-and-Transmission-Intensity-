e26e6e2f4afd3befae1d2a5cdc62cdd18a677626
Mario Quijada <141502739+Marioquijada28@users.noreply.github.com>
2025-12-27 21:35:50 -0500

    Change data source from RDS to XLSX
    Updated data source from RDS to XLSX format.

--- DELAY ANALYSES.R
+++ DELAY ANALYSES.R
@@ -30,7 +30,7 @@ library(lubridate)
 # Add data base
 # Replace your path here; using "path/to/..." as a placeholder.
 # Read the dataset from an RDS file.
-DR <- readRDS("path/to/LineList_2000_24.rds") #Delay report
+DR <- readRDS("path/to/LineList_2000_24.xlsx") #Delay report
 
 # Convert Date Strings to Date Objects
 # Ensure all date columns are formatted properly for calculations
